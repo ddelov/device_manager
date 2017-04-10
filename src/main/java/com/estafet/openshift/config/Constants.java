106447b318d34ee720ecc846f0a450d8c8e1ff0a
Delcho Delov <delcho.delov@gmail.com>
2017-04-10 11:15:15 +0300

    Initial commit

---
+++ src/main/java/com/estafet/openshift/config/Constants.java
@@ -0,0 +1,71 @@
+package com.estafet.openshift.config;
+
+//import com.estafet.openshift.model.enumerations.DeviceStatus;
+
+/**
+ * Created by Delcho Delov on 04.04.17.
+ */
+
+public interface Constants {
+		String DATE_PATTERN = "yyyyMMdd";
+
+		String SCHEMA_NAME = "openshift";
+
+		String DRIVER="org.postgresql.Driver";
+		String CONNECTION_URL = "jdbc:postgresql://"+System.getenv("DB_HOST")+":5432/"+SCHEMA_NAME;
+		String USERNAME="debil4o";
+		String PASSWORD="debil4o";
+
+		//DB tables
+		String TABLE_NAME_CUSTOMER = "customer";// id; username; password; email
+		String TABLE_NAME_DEVICE_OWNERSHIP = "dev_ownership";//
+		String TABLE_NAME_SHADOW_DATA = "shadow_data";//thing_name;stamp;reported(json);desired(json)
+		String TABLE_NAME_LEAKS_DATA = "leaks_data";
+
+		//DB columns
+		String COL_ID = "id";
+		String COL_USERNAME = "username";
+		String COL_PASSWORD = "password";
+		String COL_EMAIL = "email";
+		String COL_TSTAMP = "tstamp";
+		String COL_THING_NAME = "thing_name";
+		String COL_REPORTED = "reported";
+		String COL_DESIRED = "desired";
+		String COL_LEAK_DETECTED = "leak_detected";
+		String COL_PRESSURE = "pressure";
+		String COL_CUST_ID = "cust_id";
+		String COL_VALID_FROM = "valid_from";
+		String COL_VALID_TO = "valid_to";
+		String COL_OWN = "own";
+		String COL_THING_TYPE = "thing_type";
+		String COL_SN = "sn";
+
+		// device types
+		String TYPE_NAME_LEAKDETECTOR = "LeakDetector";
+		String TYPE_NAME_CAMERA = "Camera";
+
+		//device related
+		String DEVICE_ID = "device_id";
+//		DeviceStatus DEVICE_STATUS_DEFAULT = DeviceStatus.OFF;
+		boolean LEAK_DEFAULT = false;
+		float PRESSURE_DEFAULT = 0;
+
+		// service related
+		int CLIENT_EXECUTION_TIMEOUT = 20000;
+		String POLICY_NAME = "iotAll";
+
+		// HTTP & parameters
+		String HEADERS = "headers";
+		//		public static final String TOKEN_ID = "tokenId";
+		// header parameters should contains only
+		String HDR_CUSTOMER_ID = "customer_id";
+		String HDR_THING_NAME = "thing_name";
+		String HDR_THING_TYPE = "thing_type";
+		String ROLE = "role";
+		String ROLE_MANAGER = "manager";
+		String ROLE_CUSTOMER = "customer";
+		String NEW_DEVICE_STATUS = "action";
+		String RESPONCE_RESULT = "result";
+		String CERTIFICATE_ARN = "certificateArn";
+
+}
---
+++ src/main/java/com/estafet/openshift/rest/DeviceManagerServices.java
@@ -0,0 +1,126 @@
+package com.estafet.openshift.rest;
+
+import org.apache.log4j.Logger;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by Delcho Delov on 06.04.17.
+ */
+
+@ApplicationPath("/")
+public class DeviceManagerServices extends Application {
+		private Logger log = Logger.getLogger(DeviceManagerServices.class);
+//		private final DeviceManager deviceManager = new BaseDeviceManager();
+
+		@GET
+		@Path("/")
+		@Produces(MediaType.TEXT_PLAIN)
+		public String hello() {
+				return "Welcome to OpenShift, Mr. Delov!";
+		}
+
+
+//		@POST
+//		@Path("/addNewDevice")
+//		@Consumes(MediaType.APPLICATION_JSON)
+//		@Produces(MediaType.APPLICATION_JSON)
+//		public Response addNewDevice(String jsonPayload) {
+//				log.info("Calling DeviceManager.addNewDevice() method");
+//				Gson gson = new GsonBuilder().create();
+//				try {
+//						//1. extract input parameters
+//						final Map<String, Object> body = gson.fromJson(jsonPayload, Map.class);
+//						if (body == null || body.isEmpty()) {
+//								throw new EmptyArgumentException("Missing request body");
+//						}
+//
+//						if (!body.containsKey(COL_CUST_ID)) {
+//								log.error(COL_CUST_ID + " parameter is mandatory");
+//								throw new EmptyArgumentException(COL_CUST_ID + " parameter is mandatory");
+//						}
+//						final int customerId = (int) body.get(COL_CUST_ID);
+//
+//						final String thingName = (String) body.get(COL_THING_NAME);
+//						if (isEmpty(thingName)) {
+//								log.error(COL_THING_NAME + " parameter is mandatory");
+//								throw new EmptyArgumentException(COL_THING_NAME + " parameter is mandatory");
+//						}
+//						final String thingType = (String) body.get(COL_THING_TYPE);
+//						if (isEmpty(thingType)) {
+//								log.error(COL_THING_TYPE + " parameter is mandatory");
+//								throw new EmptyArgumentException(COL_THING_TYPE + " parameter is mandatory");
+//						}
+//						final String sn = (String) body.get(COL_SN);
+//						if (isEmpty(sn)) {
+//								log.error(COL_SN + " parameter is mandatory");
+//								throw new EmptyArgumentException(COL_SN + " parameter is mandatory");
+//						}
+//						if (!body.containsKey(COL_OWN)) {
+//								log.error(COL_OWN + " parameter is mandatory");
+//								throw new EmptyArgumentException(COL_OWN + " parameter is mandatory");
+//						}
+//						final boolean own = (boolean) body.get(COL_OWN);
+//						final String validFrom = (String) body.get(COL_VALID_FROM);
+//						if (isEmpty(validFrom)) {
+//								log.error(COL_VALID_FROM + " parameter is mandatory");
+//								throw new EmptyArgumentException(COL_VALID_FROM + " parameter is mandatory");
+//						}
+//						//2.
+//						final DeviceOwnership deviceOwnership = new DeviceOwnership(customerId, thingName, thingType, sn, own);
+//						deviceOwnership.setValidFrom(validFrom);
+//						registerDevice(deviceOwnership);
+//				}catch (DMException e){
+//						log.error(e.getMessage(), e);
+//						return Response.status(HttpServletResponse.SC_BAD_REQUEST).build();
+//				}
+//
+//				log.info("Exit DeviceManager.addNewDevice() method");
+//				// return HTTP response 200 in case of success
+//				return Response.status(HttpServletResponse.SC_OK).entity("Customer inserted").build();
+//		}
+//
+//		/**
+//		 * Executes in a transaction
+//		 * Notes:
+//		 * 1. thing type is not checked - any non-empty value is accepted
+//		 *
+//		 * @param deviceOwnership
+//		 * @throws DMException
+//		 */
+//
+//		protected void registerDevice(DeviceOwnership deviceOwnership) throws DMException {
+//				try (Connection conn = ConnectionProvider.getCon()) {
+//						try {
+//								//1. search device current record in DeviceOwnership and mark as invalid
+//								if (deviceOwnership.loadLastActive(conn)) {
+//										deviceOwnership.setValidTo(Calendar.getInstance()); // effective immediately - today device is no longer active
+//										deviceOwnership.writeToDb(conn);
+//								}
+//								String thingTypeName = deviceOwnership.getThingTypeName();
+////				listThingTypesRequest.setThingTypeName(thingTypeName);
+//								AbstractDevice device = deviceManager.createInstance(deviceOwnership.getThingName(), thingTypeName, deviceOwnership.getSn());
+//								ReportedState reportedState = new ReportedState(device.getCurrentState());
+//								final Gson json = new Gson();
+//								final String initialStatePayload = json.toJson(reportedState);
+//								log.info("intitialStateJson = " + initialStatePayload);
+//								ShadowData shadowData = new ShadowData(deviceOwnership.getThingName(), initialStatePayload, null);
+//								shadowData.writeToDb(conn);
+//								deviceOwnership.writeToDb(conn);
+//
+//								conn.commit();
+//						}catch (SQLException e){
+//								conn.rollback();
+//								throw new DMException("Could not register device ", e);
+//						}
+//				}catch (SQLException e){
+//						throw new DMException("Could not open DB connection", e);
+//				}
+//		}
+
+}
