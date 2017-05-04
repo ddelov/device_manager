1ddff53656b80fb1a2f9b66ae4f572852a6f65b6
Delcho Delov <delcho.delov@gmail.com>
2017-05-04 16:02:59 +0300

    Added empty line between queries

--- src/main/java/com/estafet/openshift/dm/config/Queries.java
+++ src/main/java/com/estafet/openshift/dm/config/Queries.java
@@ -20,8 +20,10 @@ public interface Queries {
 						"," + Constants.COL_OWN + "," + Constants.COL_VALID_FROM + "," + Constants.COL_VALID_TO +
 						"," + Constants.COL_THING_NAME + "," + Constants.COL_STATUS +
 						" ) values (?,?,?,?,?,?,?,?)";
+
 		String SQL_MARK_DEV_OWNERSHIP_INVALID = "update " + Constants.SCHEMA_NAME + '.' + Constants.TABLE_NAME_DEVICE_OWNERSHIP +
 						" set " + Constants.COL_VALID_TO + " = ? where " + Constants.COL_ID + " = ?";
+
 		String SQL_GET_ALL_DEV_OWNERSHIP = "select " + Constants.COL_ID + ", " + Constants.COL_THING_NAME + "," +
 						Constants.COL_THING_TYPE + "," + Constants.COL_SN + "," + Constants.COL_OWN + "," + Constants.COL_VALID_FROM +
 						"," + Constants.COL_VALID_TO + "," + Constants.COL_CUST_ID + "," + Constants.COL_STATUS +
@@ -31,6 +33,7 @@ public interface Queries {
 						" (" + Constants.COL_VALID_FROM + " <= ? and " + Constants.COL_VALID_TO + " is NULL )" +
 						" or (" + Constants.COL_VALID_TO + " > ? )" +
 						" )";
+
 		String SQL_GET_DEV_OWNERSHIP_BY_CUSTOMER = "select " + Constants.COL_ID + ", " + Constants.COL_THING_NAME + "," +
 						Constants.COL_THING_TYPE + "," + Constants.COL_SN + "," + Constants.COL_OWN + "," + Constants.COL_VALID_FROM +
 						"," + Constants.COL_VALID_TO + "," + Constants.COL_CUST_ID + "," + Constants.COL_STATUS +
@@ -40,6 +43,7 @@ public interface Queries {
 						" (" + Constants.COL_VALID_FROM + " <= ? and " + Constants.COL_VALID_TO + " is NULL )" +
 						" or (" + Constants.COL_VALID_TO + " > ? )" +
 						" ) and " + Constants.COL_CUST_ID + " like ?";
+
 		String SQL_CHANGE_DEV_STATUS = "update " + Constants.SCHEMA_NAME + '.' + Constants.TABLE_NAME_DEVICE_OWNERSHIP +
 						" set " + Constants.COL_STATUS + " = ? where " + Constants.COL_ID + " = ?";
 }
